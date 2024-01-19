aadf02fb2c018149cd4e30b962062452a46ff864
Suhas Abdullah <suhasabdullah43@gmail.com>
2024-01-19 15:49:40 +0600

    ready to deploy

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const chartRouter = require('./router/chartRouter')
 const headerRouter = require('./router/headerRouter')
 const footerRouter = require('./router/footerRouter')
 
-const port = 5000 ;
+// const port = 5000 ;
 
-app.listen(port,()=>{
-    console.log('server is running at port ',port)
-})
+// app.listen(port,()=>{
+//     console.log('server is running at port ',port)
+// })
 
 
 app.use('/product',productRouter)
